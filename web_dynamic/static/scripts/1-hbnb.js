dbd1bbd182097054156b3cf3c58226aa6b55f0bf
DaisyAram <daisyarama03@gmail.com>
2024-05-05 07:16:27 +0000

    updates

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,11 @@
-i
+(document).ready(function () {
+  const selectedAmenities = {};
+  $('li input[type="checkbox"]').change(function () {
+    if (this.checked) {
+             amenities[this.dataset.name] = this.dataset.id;
+   } else {
+           delete amenities[this.dataset.name];
+    }
+    $(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+ });
+});
