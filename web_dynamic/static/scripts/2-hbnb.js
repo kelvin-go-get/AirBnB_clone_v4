914ecf111b72bf4f2fba31154793c92dc90ca1b2
DaisyAram <daisyarama03@gmail.com>
2024-05-05 07:57:15 +0000

    updates

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,4 +8,11 @@
                  }
                  $(".amenities h4").text(Object.keys(amenities).sort().join(", "));
            });
+           $.getJSON("http://0.0.0.0:5001/api/v1/status/", (data) => {
+		if (data.status === "OK") {
+			$("div#api_status").addClass("available");
+		} else {
+			$("div#api_status").removeClass("available");
+		}
+	});
 });
